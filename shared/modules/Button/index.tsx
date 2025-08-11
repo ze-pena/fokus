9804cb3d775c296b57cdefc016c3c23cf5289997
ze-pena <jose.pena.dev@gmail.com>
2025-08-11 00:59:39 -0300

    feat: add alternate styling props

--- shared/modules/Button/index.tsx
+++ shared/modules/Button/index.tsx
@@ -1,29 +1,64 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 
 interface IProps {
+  icon?: React.ReactNode;
   title: string;
   onPress: () => void;
+  isOutline?: boolean;
 }
 
-export default function Button({ title, onPress }: IProps) {
+export default function Button({
+  icon,
+  title,
+  onPress,
+  isOutline = false,
+}: IProps) {
   return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.buttonText}>{title}</Text>
+    <Pressable
+      style={[
+        styles.button,
+        isOutline ? styles.button_outline : styles.button_fill,
+      ]}
+      onPress={onPress}
+    >
+      {icon && icon}
+      <Text
+        style={[
+          styles.text,
+          isOutline ? styles.text_outline : styles.text_fill,
+        ]}
+      >
+        {title}
+      </Text>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
+    flexDirection: "row",
+    gap: 16,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#b872ff",
     borderRadius: 32,
   },
-  buttonText: {
+  button_fill: {
+    backgroundColor: "#b872ff",
+  },
+  button_outline: {
+    backgroundColor: "transparent",
+    borderWidth: 2,
+    borderColor: "#b872ff",
+  },
+  text: {
     textAlign: "center",
-    color: "#021123",
     fontSize: 18,
-    padding: 8,
+    padding: 12,
+  },
+  text_fill: {
+    color: "#ffffff",
+  },
+  text_outline: {
+    color: "#b872ff",
   },
 });
