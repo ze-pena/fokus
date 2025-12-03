ab8db64826615fe176c5076069b4cb50bf9034fa
ze-pena <jose.pena.dev@gmail.com>
2025-12-03 19:30:53 -0300

    refactor: move timer page to a dedicated folder

--- app/timer/index.tsx
+++ app/timer/index.tsx
@@ -1,32 +1,33 @@
-import { IAction } from "@/shared/interfaces/Action";
 import { useState } from "react";
 import { Image, StyleSheet, View } from "react-native";
 
-import Footer from "@/shared/modules/Footer";
+import Footer from "@/shared/layout/Footer";
 import Timer from "@/shared/modules/Timer";
+
+import { IAction } from "@/shared/entities/Action";
 
 const actionList: IAction[] = [
   {
     id: "focus",
     initialValue: 25 * 60,
-    image: require("../assets/images/pomodoro/focus.png"),
+    image: require("@/assets/images/pomodoro/focus.png"),
     display: "Foco",
   },
   {
     id: "short",
     initialValue: 5 * 60,
-    image: require("../assets/images/pomodoro/pause_short.png"),
+    image: require("@/assets/images/pomodoro/pause_short.png"),
     display: "Pausa curta",
   },
   {
     id: "long",
     initialValue: 15 * 60,
-    image: require("../assets/images/pomodoro/pause_long.png"),
+    image: require("@/assets/images/pomodoro/pause_long.png"),
     display: "Pausa longa",
   },
 ];
 
-export default function Pomodoro() {
+export default function TimerPage() {
   const [currentAction, setCurrentAction] = useState(actionList[0]);
 
   return (
